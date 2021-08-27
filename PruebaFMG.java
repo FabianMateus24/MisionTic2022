2945e0c610f39c1c991b9afb69e2be541baf5a9c
FabianMateus24 <89643938+FabianMateus24@users.noreply.github.com>
2021-08-27 07:57:07 -0500

    Version 2.0
    Version 2.0

--- PruebaFMG.java
+++ PruebaFMG.java
@@ -1,8 +1,8 @@
 public class PruebaFMG{
 
-public static void name(){
+    public static void name(){
 
-system.out.print("Prueba Mision Tic 2.0");
+        system.out.print("Prueba Mision Tic 2.0");
 
 }
 
